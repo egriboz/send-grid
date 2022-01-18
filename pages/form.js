719404d71a89a61d42b170c325dbf0325f2adc42
Fatih Eğriboz <fatih.egriboz@bilgi.edu.tr>
2022-01-18 12:39:51 +0300

    sample code

--- pages/form.js
+++ pages/form.js
@@ -3,20 +3,20 @@ import sendgrid from '@sendgrid/mail';
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default async (req, res) => {
-  try {
-    await sendgrid.send({
-      to: 'egriboz@gmail.com',
-      from: 'egriboz@gmail.com',
-      subject: 'Serverless Functions',
-      text: 'Hello, world!'
-    });
-  } catch (error) {
-    return res.status(error.statusCode || 500).json({ error: error.message });
-  }
+// export default async (req, res) => {
+//   try {
+//     await sendgrid.send({
+//       to: 'egriboz@gmail.com',
+//       from: 'egriboz@gmail.com',
+//       subject: 'Serverless Functions',
+//       text: 'Hello, world!'
+//     });
+//   } catch (error) {
+//     return res.status(error.statusCode || 500).json({ error: error.message });
+//   }
 
-  return res.status(200).json({ error: '' });
-};
+//   return res.status(200).json({ error: '' });
+// };
 
 export default function Home() {
   return (
